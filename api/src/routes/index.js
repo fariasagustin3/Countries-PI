95a8f631ed590da2fe5c6b810f77807dba901faa
fariasagustin3 <fariasagustin3@gmail.com>
2022-05-27 17:02:13 -0300

    middlewares setted

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,20 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const router = Router();
+const morgan = require('morgan');
+const countries = require('./countries');
+const activities = require('./activities');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
-const router = Router();
-
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.use(morgan('dev'));
+
+router.use('/countries', countries);
+router.use('/activities', activities);
 
 
 module.exports = router;
