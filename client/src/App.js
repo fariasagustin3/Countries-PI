b168479caccb30bc3aa693d2879ec14d4c390201
fariasagustin3 <fariasagustin3@gmail.com>
2022-05-31 21:13:31 -0300

    new Route created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import Home from './components/Home';
+import CreateActivity from './components/CreateActivity';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path='/' component={ LandingPage } />
+        <Route path='/home' component={ Home } />
+        <Route path='/activities' component={ CreateActivity } />
+      </div>
+    </BrowserRouter>
   );
 }
 
