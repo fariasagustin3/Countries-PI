a25600faaea4ae34c19f4ac624bc3ca5b2fb0265
fariasagustin3 <fariasagustin3@gmail.com>
2022-06-06 12:18:40 -0300

    post activity action creator modified successfully

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,12 +6,14 @@ import {
   ORDER_BY_NAME,
   GET_ACTIVITIES,
   COUNTRY_BY_NAME,
-  POST_ACTIVITY
+  POST_ACTIVITY,
+  GET_DETAILS,
+  ORDER_BY_POPULATION
 } from './actionTypes';
 
 export function getAllCountries() {
   return async function(dispatch) {
-    const countries = (await axios.get(`http://localhost:3001/countries`))
+    const countries = await axios.get(`http://localhost:3001/countries`)
     return dispatch({ type: GET_ALL_COUNTRIES, payload: countries.data })
   }
 }
@@ -37,9 +39,16 @@ export function orderByName(payload) {
   }
 }
 
+export function orderByPopulation(payload) {
+  return {
+    type: ORDER_BY_POPULATION,
+    payload
+  }
+}
+
 export function getActivities() {
   return async function(dispatch) {
-    const activities = (await axios.get(`http://localhost:3001/activities`))
+    const activities = await axios.get(`http://localhost:3001/activities`)
     return dispatch({ type: GET_ACTIVITIES, payload: activities.data })
   }
 }
@@ -55,5 +64,12 @@ export function postActivity(payload) {
   return async function(dispatch) {
     const activities = await axios.post(`http://localhost:3001/activities`, payload)
     return activities;
+  }
+}
+
+export function getDetails(id) {
+  return async function(dispatch) {
+    const country = await axios.get(`http://localhost:3001/countries/${id}`)
+    return dispatch({ type: GET_DETAILS, payload: country.data })
   }
 }
