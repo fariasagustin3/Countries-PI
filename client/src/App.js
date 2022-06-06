c5d7c75fa8d5af60968c188a018f1c738d471efc
fariasagustin3 <fariasagustin3@gmail.com>
2022-06-06 12:17:06 -0300

    Route added to render countries details

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Home from './components/Home';
 import CreateActivity from './components/CreateActivity';
+import Detail from './components/Detail';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/' component={ LandingPage } />
         <Route path='/home' component={ Home } />
         <Route path='/activities' component={ CreateActivity } />
+        <Route path='/detail/:id' component={ Detail } />
       </div>
     </BrowserRouter>
   );
