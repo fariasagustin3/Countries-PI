ff3d1d04c9e4cdef02b1f7036c4e6075e3f79603
fariasagustin3 <fariasagustin3@gmail.com>
2022-06-06 12:19:24 -0300

    css added

--- client/src/components/Country.jsx
+++ client/src/components/Country.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles/Country.css'
 
 
 function Country({ id, name, flag, continent }) {
   return(
     <div className='container'>
-      <Link to={`/countryDetail/${id}`} >
+      <Link to={`detail/${id}`}>
         <div className='country-container'>
           <div className='container-flag'>
             <img src={`${flag}`} alt='image not found' className='flag' width='300px' height='180px' />
           </div>
-          <span className='name'>{name}</span>
+          <span className='country-name'>{name}</span>
           <span className='continent'>{continent}</span>
         </div>
       </Link>
