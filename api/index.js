e16263a6f48830e635eba99d005e59a76cc1f534
fariasagustin3 <fariasagustin3@gmail.com>
2022-07-08 20:22:44 -0300

    last pushing

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { apiInfo } = require('./src/routes/controllers/controllers');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, async () => {
+  server.listen(3001, async () => {
     await apiInfo();
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
