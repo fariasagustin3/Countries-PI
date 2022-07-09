20a5198b9718e1963b6797796d5f2775016fac64
fariasagustin3 <fariasagustin3@gmail.com>
2022-07-09 01:44:07 -0300

    last commit deploy

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,7 +14,7 @@ import {
 
 export function getAllCountries() {
   return async function(dispatch) {
-    const countries = await axios(`http://localhost:3001/countries`)
+    const countries = await axios(`/countries`)
     return dispatch({ type: GET_ALL_COUNTRIES, payload: countries.data })
   }
 }
@@ -56,28 +56,28 @@ export function orderByPopulation(payload) {
 
 export function getActivities() {
   return async function(dispatch) {
-    const activities = await axios.get(`http://localhost:3001/activities`)
+    const activities = await axios.get(`/activities`)
     return dispatch({ type: GET_ACTIVITIES, payload: activities.data })
   }
 }
 
 export function countryByName(name) {
   return async function(dispatch) {
-    const country = await axios.get(`http://localhost:3001/countries/name?name=${name}`);
+    const country = await axios.get(`/countries/name?name=${name}`);
     return dispatch({ type: COUNTRY_BY_NAME, payload: country.data })
   }
 }
 
 export function postActivity(payload) {
   return async function(dispatch) {
-    const activities = await axios.post(`http://localhost:3001/activities`, payload)
+    const activities = await axios.post(`/activities`, payload)
     return activities;
   }
 }
 
 export function getDetails(id) {
   return async function(dispatch) {
-    const country = await axios.get(`http://localhost:3001/countries/${id}`)
+    const country = await axios.get(`/countries/${id}`)
     return dispatch({ type: GET_DETAILS, payload: country.data })
   }
 }
